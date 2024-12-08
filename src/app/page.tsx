72308e6070d8122b059bb90c1db920522b3c8316
Afsal Khan <afsalkhanmdt@gmail.com>
2024-12-08 20:56:27 +0530

    feat: add ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,68 @@
-import Image from "next/image";
+import Link from "next/link";
+
+const list =
+  [
+    {
+      title: "The Whispering Forest",
+      author: "Emily R. Wells",
+      genre: "Fantasy",
+      content: "A young girl ventures into a mysterious forest where ancient secrets and mythical creatures await."
+    },
+    {
+      title: "Echoes of the Past",
+      author: "John H. Carter",
+      genre: "Historical Fiction",
+      content: "A time traveler must uncover the truth about a long-forgotten betrayal that could change history."
+    },
+    {
+      title: "The Lost Key",
+      author: "Samantha D. Lee",
+      genre: "Mystery",
+      content: "A detective races against time to solve a series of cryptic clues that lead to a hidden treasure."
+    },
+    {
+      title: "A Heart of Steel",
+      author: "Michael T. Grant",
+      genre: "Science Fiction",
+      content: "In a dystopian future, a soldier with a mechanical heart must choose between loyalty and love."
+    },
+    {
+      title: "The Silent Tide",
+      author: "Anna W. Green",
+      genre: "Thriller",
+      content: "A small coastal town is rocked by a string of mysterious disappearances, with no clues left behind."
+    },
+    {
+      title: "Starlight Dreams",
+      author: "Clara F. Adams",
+      genre: "Romance",
+      content: "Two souls separated by fate must navigate a world of dreams and reality to find each other again."
+    },
+    {
+      title: "The Hidden Chamber",
+      author: "Lucas D. Flynn",
+      genre: "Adventure",
+      content: "An archaeologist uncovers a lost chamber beneath the city, filled with ancient artifacts and dangers."
+    }
+  ]
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <header className="p-2 shadow">
+        <div className=" max-w-4xl m-auto">
+          <div className=" text-green-900 font-bold text-2xl">TaleNest</div>
         </div>
+      </header>
+      <main className="p-2 grid gap-2 max-w-4xl m-auto">
+        {list.map((story) =>
+          <Link href={'/tale/hello'} key={story.title} className="shadow p-2">
+            <div className="text-lg text-slate-900">{story.title}</div>
+            <div className=" text-gray-600">{story.content}</div>
+          </Link>
+        )
+        }
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ src/app/tale/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+
+const story =
+{
+    title: "The Whispering Forest",
+    author: "Emily R. Wells",
+    genre: "Fantasy",
+    content: [
+        "Once upon a time, in the tranquil town of Meadowfield, nestled gracefully within the folds of nature, where life moved at a gentler pace, a story unfolded that filled everyone's heart with hope and courage.",
+        "The protagonist of our story is Rose, a twelve-year-old girl. Rose's life in Meadowfield had been simple, she lived in a secluded cottage, maintaining her family's old cemetery ground. For years, it was just them, but solitude didn't haunt their happiness for they cherished each other's company immensely.",
+        "One day, Rose found an odd-looking key in the cemetery ground. It was unlike any she had seen before. The key was old, a peculiar design etched into its handle. Intrigued, she held on to it.",
+        "That night, a dreadful storm hit Meadowfield, causing widespread devastation. The winds howled, and the thunder roared as if Mother Nature herself was mourning. In the chaos, a massive old tree from the cemetery grounds toppled onto their dainty cottage. Their home was destroyed, their heart, however, remained resilient.",
+        "The miserable morning light revealed the destruction. Yet, in this devastation, the uprooted tree had unveiled a secret - a hidden rusted door that laid beneath it. That’s when Rose remembered the key. With a deep breath and newfound courage, she pushed the heavy door open.",
+        "Inside, it was dark and damp. As her eyes adjusted to the darkness, Rose saw something that turned her fear into amazement. It was a room filled with endless scrolls and ancient artifacts, like a forgotten treasury of wisdom from a bygone era. In the corner was a mysterious glowing scroll that seemed to call out to Rose, guiding her forward. She opened it and her eyes widened, it was a map of Meadowfield but with countless secrets marked out.",
+        "In the days that followed, curiosity led Rose to venture where the map guided, unearthing these buried cues – old relics, forgotten stories, secret spaces, and more. Each discovery rekindled hope in her heart and the townsfolk. Slowly but surely, they started rebuilding the town, with Rose's map as their guide. As each day passed, Meadowfield's charm was restored, along with the morale of its people.",
+        "On her journey of discovery, Rose was no longer a mere cemetery keeper's daughter, she became a beacon of resilience and courage. She brought the town back to life not just through physical means, but by instilling a newfound sense of community and hope.",
+        "By the end of the year, Meadowfield was more vibrant than ever. It was a testament to the town's resilience, to human spirit, and to Rose, the silent guardian who turned her destiny around. The people celebrated their survival and newfound unity. As for Rose, the little girl who lived by the cemetery became a legend, the savior of Meadowfield.",
+        "The tale of Meadowfield and Rose is not just a story but a timeless reminder. It professes that even the worst storms are always followed by a bright dawn and those who hold on and fight against the odds, no matter how small or insignificant they might seem, find strength in themselves that they never knew existed."
+    ]
+}
+
+
+export default async function Tale({ params }: { params: Promise<{ slug: string }> }) {
+    console.log((await params).slug);
+
+    return (
+        <div>
+            <header className="p-2 shadow" >
+                <div className=" max-w-4xl m-auto" >
+                    <Link href={'/'} className=" text-green-900 font-bold text-2xl" > TaleNest </Link>
+                </div>
+            </header>
+            < main className="p-2 grid gap-2 max-w-4xl m-auto" >
+                <div key={story.title} className="shadow p-4" >
+                    <div className="text-xl text-slate-900 pb-4 font-bold" > {story.title} </div>
+                    < div className=" text-gray-600 grid gap-4 text-lg" > {
+                        story.content.map((text, i) =>
+                            <div key={i}>
+                                {text}
+                            </div>
+                        )
+                    } </div>
+                </div>
+            </main>
+        </div>
+    );
+}
