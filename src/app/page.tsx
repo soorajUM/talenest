9be64cb3dcea79b1fa3af2f5f702b6783cf8f0ba
Afsal Khan <afsalkhanmdt@gmail.com>
2024-12-11 18:34:41 +0530

    fix: sorting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,55 +2,11 @@ import Link from "next/link";
 import connectDB from "../lib/connectDb";
 import Tale from "../models/tale";
 
-const list =
-  [
-    {
-      title: "The Whispering Forest",
-      author: "Emily R. Wells",
-      genre: "Fantasy",
-      content: "A young girl ventures into a mysterious forest where ancient secrets and mythical creatures await."
-    },
-    {
-      title: "Echoes of the Past",
-      author: "John H. Carter",
-      genre: "Historical Fiction",
-      content: "A time traveler must uncover the truth about a long-forgotten betrayal that could change history."
-    },
-    {
-      title: "The Lost Key",
-      author: "Samantha D. Lee",
-      genre: "Mystery",
-      content: "A detective races against time to solve a series of cryptic clues that lead to a hidden treasure."
-    },
-    {
-      title: "A Heart of Steel",
-      author: "Michael T. Grant",
-      genre: "Science Fiction",
-      content: "In a dystopian future, a soldier with a mechanical heart must choose between loyalty and love."
-    },
-    {
-      title: "The Silent Tide",
-      author: "Anna W. Green",
-      genre: "Thriller",
-      content: "A small coastal town is rocked by a string of mysterious disappearances, with no clues left behind."
-    },
-    {
-      title: "Starlight Dreams",
-      author: "Clara F. Adams",
-      genre: "Romance",
-      content: "Two souls separated by fate must navigate a world of dreams and reality to find each other again."
-    },
-    {
-      title: "The Hidden Chamber",
-      author: "Lucas D. Flynn",
-      genre: "Adventure",
-      content: "An archaeologist uncovers a lost chamber beneath the city, filled with ancient artifacts and dangers."
-    }
-  ]
-
 export default async function Home() {
   await connectDB();
-  const tales = await Tale.find().limit(20);
+  const tales = await Tale.aggregate(
+    [{ $sample: { size: 20 } }]
+  );
 
   return (
     <div>
@@ -63,7 +19,7 @@ export default async function Home() {
         {tales.map((story) =>
           <Link href={`/tale/${story.slug}`} key={story._id} className="shadow p-2">
             <div className="text-lg text-slate-900">{story.title}</div>
-            <div className=" text-gray-600">{story.content[0]}</div>
+            <div className=" text-gray-600 line-clamp-3">{story.content[0]}</div>
           </Link>
         )
         }
