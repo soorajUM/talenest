7328e8e52921f586732d3155cb6a833546a4ec8f
Afsal Khan <afsalkhanmdt@gmail.com>
2024-12-24 20:43:30 +0530

    feat: google analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export default function RootLayout({
@@ -9,6 +10,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-MJQPJSSJF1" />
+        <Script dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MJQPJSSJF1');`
+        }} />
       </head>
       <body>
         {children}
