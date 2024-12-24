3cdfebca080787f8278293b1eab62fe68318ae72
Afsal Khan <afsalkhanmdt@gmail.com>
2024-12-24 20:53:33 +0530

    feat: add clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,20 @@ export default function RootLayout({
           gtag('js', new Date());
           gtag('config', 'G-MJQPJSSJF1');`
           }} />
+        <Script
+          type="text/javascript"
+          id='clarity'
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "pj1lkszyll");
+              `
+          }}
+        />
+
       </head>
       <body>
         {children}
