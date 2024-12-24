77f4820498bff4f488c8278765b43caa6f6cb093
Afsal Khan <afsalkhanmdt@gmail.com>
2024-12-24 20:45:59 +0530

    fix: id of script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-MJQPJSSJF1" />
-        <Script dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
+        <Script id='google_tag_manager' async src="https://www.googletagmanager.com/gtag/js?id=G-MJQPJSSJF1" />
+        <Script
+          id='ga_data_layer'
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-MJQPJSSJF1');`
-        }} />
+          }} />
       </head>
       <body>
         {children}
